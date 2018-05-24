26a03c1a45bd461661cc91842e739794f53e7503
wangzehua <zehuawong@163.com>
2018-05-24 17:54:38 +0800

    first commit

---
+++ app/src/main/java/com/wang/customtitlebar/TopTitleBar.java
@@ -0,0 +1,14 @@
+package com.wang.customtitlebar;
+
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class TopTitleBar extends RelativeLayout{
+
+    private Button leftBtn;
+    private Button rightBtn;
+    private TextView titleTextview;
+
+
+}
