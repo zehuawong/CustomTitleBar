755a7899ce2d2d7a3745948c67e5d506557d4962
wangzehua <zehuawong@163.com>
2018-05-25 17:22:03 +0800

    修改状态栏透明和颜色

--- app/src/main/java/com/wang/customtitlebar/MainActivity.java
+++ app/src/main/java/com/wang/customtitlebar/MainActivity.java
@@ -1,11 +1,14 @@
 package com.wang.customtitlebar;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import java.lang.ref.WeakReference;
@@ -48,6 +51,17 @@ public class MainActivity extends Activity {
         mhandler=new MyHandler(this);
 
         initView();
+
+
+        Window window = this.getWindow();
+        //取消设置透明状态栏,使 ContentView 内容不再覆盖状态栏
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        //设置状态栏颜色
+        window.setStatusBarColor(Color.parseColor("#f88b00"));
+
+
     }
 
     private void  initView(){
